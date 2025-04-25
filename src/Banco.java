4e9cd192a5d1859be1f7cc0ae203ef790bb4579d
predo505 <pedroesquecido1@gmail.com>
2025-04-25 15:07:57 -0300

    Update Banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,10 +1,37 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Banco {
+    private ArrayList<ContaBancaria> contas;
 
+    public Banco() {
+        contas = new ArrayList<>();
+    }
 
+    public void adicionarConta(ContaBancaria conta) {
+        contas.add(conta);
+    }
 
+    public ContaBancaria buscarConta(String numeroConta) {
+        for (ContaBancaria conta : contas) {
+            if (conta.getNumeroConta().equals(numeroConta)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void listarContas() {
+        for (ContaBancaria conta : contas) {
+            conta.exibirResumo();
+        }
+    }
+
+    public boolean contaJaExiste(String numeroConta, String titular) {
+        for (ContaBancaria conta : contas) {
+            if (conta.getNumeroConta().equals(numeroConta) || conta.getTitular().equalsIgnoreCase(titular)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
